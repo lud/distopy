4e4c333b2bf44bebfb6ffba01a4a86f516d8ffa5
lud <ludovic@demblans.com>
2021-10-18 12:49:53 +0200

    use --no flags in fixer

--- lib/distopy.ex
+++ lib/distopy.ex
@@ -26,6 +26,9 @@ defmodule Distopy do
     check_extra = Keyword.get(opts, :extra) != false
     check_missing = Keyword.get(opts, :missing) != false
 
+    check_extra |> IO.inspect(label: ~S[check_extra])
+    check_missing |> IO.inspect(label: ~S[check_missing])
+
     invalid? = (check_missing and length(missing) > 0) or (check_extra and length(extra) > 0)
 
     if check_missing and length(missing) > 0,
@@ -54,14 +57,31 @@ defmodule Distopy do
     opts = Keyword.put_new(opts, :show_fix_opt, false)
 
     with :error <- diff_and_output(dist, env, diff, opts),
-         {:ok, {dist, env}} <- run_fixer(dist, env, diff) do
+         {:ok, {dist, env}} <- run_fixer(dist, env, diff, opts) do
       diff_and_output(dist, env, diff_keys(dist, env), opts)
     end
   end
 
-  def run_fixer(dist, env, %{missing: missing, extra: extra}) do
-    {:ok, {dist, env}} = CLI.fix_missing(missing, dist, env)
-    {:ok, {dist, env}} = CLI.fix_extra(extra, dist, env)
+  def run_fixer(dist, env, %{missing: missing, extra: extra}, opts) do
+    check_missing = Keyword.get(opts, :missing) != false
+    check_extra = Keyword.get(opts, :extra) != false
+
+    {dist, env} =
+      if check_missing do
+        {:ok, {dist, env}} = CLI.fix_missing(missing, dist, env)
+        {dist, env}
+      else
+        {dist, env}
+      end
+
+    {dist, env} =
+      if check_extra do
+        {:ok, {dist, env}} = CLI.fix_extra(extra, dist, env)
+        {dist, env}
+      else
+        {dist, env}
+      end
+
     {:ok, {dist, env}}
   end
 end
