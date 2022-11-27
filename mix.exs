f7912143075a955831e04e147c00bcaba8fc8c30
lud <ludovic@demblans.com>
2022-11-27 16:36:21 +0100

    new version 0.2.9

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Distopy.MixProject do
       app: :distopy,
       consolidate_protocols: false,
       package: package(),
-      version: "0.2.8",
+      version: "0.2.9",
       elixir: "~> 1.12",
       start_permanent: false,
       deps: deps(),
